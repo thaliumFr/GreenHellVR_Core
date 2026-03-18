2dc8bcafbe96cebc435c28f4b1876dc19fc54fd4
ThaliumFr <theomaxmi34@gmail.com>
2026-03-18 12:44:10 +0100

    Debug Monkey functions got split up in another file to clean up the Core Mod Object

--- Items/CoreModObject.cs
+++ Items/CoreModObject.cs
@@ -6,19 +6,13 @@ using UnityEngine.XR;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
-#pragma warning disable CS0436 // Type conflicts with imported type
-
 namespace GreenHellVR_Core.Items
 {
     public class CoreModObject : MonoBehaviour
     {
         public static CoreModObject Instance { get; private set; }
-        AssetBundle assetBundle;
 
-        GameObject monkey;
         GameObject debugMenuGO;
-
-        Material debugMaterial;
 
         public static CoreModObject Get()
         {
@@ -49,96 +43,6 @@ namespace GreenHellVR_Core.Items
             instance.debugMenuGO.transform.rotation = Quaternion.identity * Quaternion.Euler(new Vector3(-90f, 0f, 0f));*/
         }
 
-        void GetMonkeyBundle(Action<AssetBundle> action) {
-            if (Instance.assetBundle == null)
-            {
-                StartCoroutine(GHVRC_Objects.LoadAssetBundleAsync("GreenHellVR_Core.monkeybundle", (bundle) =>
-                {
-                    Instance.assetBundle = bundle;
-                    action(bundle);
-                }));
-            }
-            else
-            {
-                action(Instance.assetBundle);
-            }
-        }
-
-        void LoadMonkey()
-        {
-            GetMonkeyBundle((assetBundle) =>
-            {
-                if (assetBundle != null && Instance.monkey == null)
-                {
-                    StartCoroutine(GHVRC_Objects.LoadAssetFromBundleAsync<GameObject>(assetBundle, "monkey", (monkey) => Instance.monkey ??= monkey));
-                    StartCoroutine(GHVRC_Objects.LoadAssetFromBundleAsync<Material>(assetBundle, "rainbow.mat", (mat) => Instance.debugMaterial ??= mat));
-                }
-            });
-        }
-
-        /// <summary>
-        /// Just spawns in front of the player the famous Blender3D monkey model
-        /// </summary>
-        private void SpawnMonkey()
-        {
-            if (Instance.monkey == null)
-            {
-                Plugin.Log.LogInfo("no monkey found, attempting to reload monkey...");
-                LoadMonkey();
-
-                if (Instance.monkey == null)
-                {
-                    Plugin.Log.LogError("monkey reload failed");
-                    return;
-                }
-
-                Plugin.Log.LogMessage("monkey reload successful");
-            }
-
-            Transform playerTransform = Player.Get().GetHeadTransform();
-            float distance = 1f;
-
-            Logger.Log("Instatiate Monkey");
-            GameObject monkeyGO = Instantiate(Instance.monkey, playerTransform.position + distance * playerTransform.forward.normalized, Quaternion.identity * Quaternion.Euler(new Vector3(-90f, 0f, 0f)));
-            //monkeyGO.transform.localScale *= 0.25f;
-            monkeyGO.GetComponent<Renderer>().material = Instance.debugMaterial;
-
-            List<GrabPoint> grabPoints = [];
-            foreach (Transform child in monkeyGO.transform)
-            {
-                GrabPoint gp = child.gameObject.AddComponent<GrabPoint>();
-                gp.HandPose = HandPoseId.Default;
-                gp.MaxDegreeDifferenceAllowed = 60f;
-                gp.enabled = true;
-                grabPoints.Add(gp);
-            }
-
-            Plugin.Log.LogInfo($"Found {grabPoints.Count} grab points on monkey");
-
-            GuidComponent guidComp = monkeyGO.AddComponent<GuidComponent>();
-
-
-            Item item = monkeyGO.AddComponent<Item>();
-
-
-            //item.m_Info = itemInfo;
-            
-            item.m_GUID = guidComp.GetGuid().ToString();
-            item.m_InfoName = "Monkey";
-
-
-            item.Initialize(false);
-            ItemsManager.Get().RegisterItem(item);
-
-
-            Grabbable grab = monkeyGO.AddComponent<Grabbable>();
-            grab.DualGrabSupport = false;
-            grab.GrabPoints = grabPoints;
-            grab.Initialize();
-
-            Logger.Log($"monkey spawned at {monkeyGO.transform.position}");
-        }
-
         public void Update()
         {
             List<InputDevice> devices = [];
@@ -152,7 +56,7 @@ namespace GreenHellVR_Core.Items
             {
                 if (Input.GetKeyDown(KeyCode.M) ||((devices.Count == 1 && devices[0].TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool isPressed) && isPressed)))
                 {
-                    SpawnMonkey();
+                    DebugMonkey.SpawnMonkey();
                 }
             }
             
---
+++ Items/DebugMonkey.cs
@@ -0,0 +1,111 @@
+﻿using GreenHellVR_Core_Include.Items.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GreenHellVR_Core.Items
+{
+    internal class DebugMonkey
+    {
+        static AssetBundle ModAssetBundle;
+
+        static GameObject monkey;
+        static Material debugMaterial;
+
+        static void GetMonkeyBundle(Action<AssetBundle> action)
+        {
+            if (ModAssetBundle == null)
+            {
+                CoreModObject.Instance.StartCoroutine(GHVRC_Objects.LoadAssetBundleAsync("GreenHellVR_Core.monkeybundle", (bundle) =>
+                {
+                    ModAssetBundle = bundle;
+                    action(bundle);
+                }));
+            }
+            else
+            {
+                action(ModAssetBundle);
+            }
+        }
+
+        static void LoadMonkey()
+        {
+            GetMonkeyBundle((assetBundle) =>
+            {
+                if (assetBundle != null && monkey == null)
+                {
+                    CoreModObject.Instance.StartCoroutine(GHVRC_Objects.LoadAssetFromBundleAsync<GameObject>(assetBundle, "monkey", (monkey) => monkey ??= monkey));
+                    CoreModObject.Instance.StartCoroutine(GHVRC_Objects.LoadAssetFromBundleAsync<Material>(assetBundle, "rainbow.mat", (mat) => debugMaterial ??= mat));
+                }
+            });
+        }
+
+
+        /// <summary>
+        /// Just spawns in front of the player the famous Blender3D monkey model
+        /// </summary>
+        internal static void SpawnMonkey()
+        {
+            if (monkey == null)
+            {
+                Plugin.Log.LogInfo("no monkey found, attempting to load monkey...");
+                LoadMonkey();
+
+                if (monkey == null)
+                {
+                    Plugin.Log.LogError("monkey reload failed");
+                    return;
+                }
+
+                Plugin.Log.LogMessage("monkey reload successful");
+            }
+
+            Transform playerTransform = Player.Get().GetHeadTransform();
+            float distance = 1.5f;
+
+            Logger.Log("Instatiate Monkey");
+            GameObject monkeyGO = UnityEngine.Object.Instantiate(monkey, playerTransform.position + distance * playerTransform.forward.normalized, Quaternion.identity * Quaternion.Euler(new Vector3(-90f, 0f, 0f)));
+            monkeyGO.transform.localScale *= 0.5f;
+            monkeyGO.GetComponent<Renderer>().material = debugMaterial;
+
+            List<GrabPoint> grabPoints = [];
+            foreach (Transform child in monkeyGO.transform)
+            {
+                GrabPoint gp = child.gameObject.AddComponent<GrabPoint>();
+                gp.HandPose = HandPoseId.Notebook;
+                gp.MaxDegreeDifferenceAllowed = 60f;
+                gp.enabled = true;
+                grabPoints.Add(gp);
+            }
+
+            Plugin.Log.LogInfo($"Found {grabPoints.Count} grab points on monkey");
+
+            GuidComponent guidComp = monkeyGO.AddComponent<GuidComponent>();
+
+
+            Item item = monkeyGO.AddComponent<Item>();
+
+
+            //item.m_Info = itemInfo;
+
+            item.m_GUID = guidComp.GetGuid().ToString();
+            item.m_InfoName = "Monkey";
+
+
+            item.Initialize(false);
+            ItemsManager.Get().RegisterItem(item);
+
+
+            Grabbable grab = monkeyGO.AddComponent<Grabbable>();
+            grab.DualGrabSupport = true;
+            grab.GrabPoints = grabPoints;
+            grab.grabType = GrabAttachType.GrabPoint;
+            grab.Initialize();
+
+            Logger.Log($"monkey spawned at {monkeyGO.transform.position}");
+        }
+    }
+}
